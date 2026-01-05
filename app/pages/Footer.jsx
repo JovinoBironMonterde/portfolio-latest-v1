ddc11e3c6822636f2ffbb798c91f2685c0fc8492
Jovino Monterde <jovinobironmonterde@gmail.com>
2026-01-05 17:22:08 +0800

    Updated Project page and footer

--- app/pages/Footer.jsx
+++ app/pages/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='relative w-full h-auto flex justify-center items-center'>
       <div className="w-full h-auto">
-      <div className="w-full h-[200px] bg-zinc-600">
+      <div className="w-full h-[80px] bg-zinc-600">
         
       </div>
       <div className="  text-xs px-2 py-4 text-center text-neutral-400 bg-zinc-700">
