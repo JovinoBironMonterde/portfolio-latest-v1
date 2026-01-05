e546c055055e40ff2e542166e7891f8b3b9884d6
Jovino Monterde <jovinobironmonterde@gmail.com>
2026-01-05 18:21:14 +0800

    Updated Project page and footer v4

--- app/page.js
+++ app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
             <Aboutpage />
           </section>
 
-          <section id="profession" className="bg-sky-950 py-20 px-4">
+          <section id="profession" className="bg-sky-950 py-10 xl:py-20 px-4">
             <Profession />
           </section>
 
